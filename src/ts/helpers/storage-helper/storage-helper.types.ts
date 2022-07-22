1e14f258807635dedb009441f7a3d610b4bc8b2d
nicolaiheuck <nicolaiheuck@gmail.com>
2022-07-22 19:34:45 +0200

    Configured Prettier

--- src/ts/helpers/storage-helper/storage-helper.types.ts
+++ src/ts/helpers/storage-helper/storage-helper.types.ts
@@ -1,8 +1,8 @@
 //NH_TODO: Replace all any's with models when they are made
 export type StorageHelperType = {
-  keyExistsAsync: (key: string) => Promise<boolean>;
-  setValueAsync: (key: string, value: any) => Promise<void>;
-  getValueAsync: (key: string) => Promise<any>;
-  getProfilesAsync: () => Promise<any>;
-  getFormDataAsync: () => Promise<any>;
+	keyExistsAsync: (key: string) => Promise<boolean>;
+	setValueAsync: (key: string, value: any) => Promise<void>;
+	getValueAsync: (key: string) => Promise<any>;
+	getProfilesAsync: () => Promise<any>;
+	getFormDataAsync: () => Promise<any>;
 };
